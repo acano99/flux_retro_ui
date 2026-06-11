dd0be5e4f63cf1d4b3096635eedfc9258c825f3f
abrahan <abrahan.jcano99@gmail.com>
2026-06-11 02:58:06 +0200

    Creacion del tema del paquete con una paleta por defecto

---
+++ lib/src/theme/flux_retro_theme.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class FluxRetroTheme extends ThemeExtension<FluxRetroTheme> {
+  final Color background;
+  final Color outline;
+  final Color primary;
+  final Color secondary;
+  final Color tertiary;
+  final Color warning;
+  final Color error;
+  final Color surface;
+
+  const FluxRetroTheme({
+    required this.background,
+    required this.outline,
+    required this.primary,
+    required this.secondary,
+    required this.tertiary,
+    required this.warning,
+    required this.error,
+    required this.surface,
+  });
+
+  factory FluxRetroTheme.defaultDark() => const FluxRetroTheme(
+    background: Color(0xFF121210),
+    outline: Color(0xFF5A5A4A),
+    primary: Color(0xFF3E4F3C),
+    secondary: Color(0xFF3C4F5E),
+    tertiary: Color(0xFF9E5630),
+    warning: Color(0xFFB87D30),
+    error: Color(0xFF8B3A2B),
+    surface: Color(0xFFD9C5A0),
+  );
+
+  factory FluxRetroTheme.defaultLight() => const FluxRetroTheme(
+    background: Color(0xFFF2E8D5),
+    outline: Color(0xFF8C7E6A),
+    surface: Color(0xFFFFF8EA),
+    primary: Color(0xFF607255),
+    secondary: Color(0xFF5A7382),
+    tertiary: Color(0xFFB86F45),
+    warning: Color(0xFFC99645),
+    error: Color(0xFFA34C3F),
+  );
+
+  @override
+  FluxRetroTheme copyWith({
+    Color? background,
+    Color? outline,
+    Color? primary,
+    Color? secondary,
+    Color? tertiary,
+    Color? warning,
+    Color? error,
+    Color? surface,
+  }) {
+    return FluxRetroTheme(
+      background: background ?? this.background,
+      outline: outline ?? this.outline,
+      primary: primary ?? this.primary,
+      secondary: secondary ?? this.secondary,
+      tertiary: tertiary ?? this.tertiary,
+      warning: warning ?? this.warning,
+      error: error ?? this.error,
+      surface: surface ?? this.surface,
+    );
+  }
+
+  @override
+  FluxRetroTheme lerp(ThemeExtension<FluxRetroTheme>? other, double t) {
+    if (other is! FluxRetroTheme) return this;
+    return FluxRetroTheme(
+      background: Color.lerp(background, other.background, t)!,
+      outline: Color.lerp(outline, other.outline, t)!,
+      primary: Color.lerp(primary, other.primary, t)!,
+      secondary: Color.lerp(secondary, other.secondary, t)!,
+      tertiary: Color.lerp(tertiary, other.tertiary, t)!,
+      warning: Color.lerp(warning, other.warning, t)!,
+      error: Color.lerp(error, other.error, t)!,
+      surface: Color.lerp(surface, other.surface, t)!,
+    );
+  }
+}
+
+extension FluxRetroThemeX on BuildContext {
+  FluxRetroTheme get fluxRetro =>
+      Theme.of(this).extension<FluxRetroTheme>() ??
+      FluxRetroTheme.defaultDark();
+}
