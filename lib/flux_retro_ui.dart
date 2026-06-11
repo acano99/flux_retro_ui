6220b02ba0df360596b22c1db5ffda9acd3c0744
abrahan <abrahan.jcano99@gmail.com>
2026-06-11 07:57:53 +0200

    SliverAppBar y su exportacion

--- lib/flux_retro_ui.dart
+++ lib/flux_retro_ui.dart
@@ -5,4 +5,5 @@ export 'src/theme/flux_retro_theme.dart';
 // Widgets
 export 'src/widgets/flux_retro_buton.dart';
 export 'src/widgets/flux_retro_container.dart';
+export 'src/widgets/flux_retro_sliver_appbar.dart';
 export 'src/widgets/retro_scaffold_body.dart';
---
+++ lib/src/widgets/flux_retro_sliver_appbar.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flux_retro_ui/flux_retro_ui.dart';
+
+class FluxRetroSliverAppbar extends StatelessWidget {
+  const FluxRetroSliverAppbar({super.key, required this.title, this.actions});
+
+  final String title;
+  final List<Widget>? actions;
+
+  @override
+  Widget build(BuildContext context) {
+    final retroTheme = context.fluxRetro;
+
+    return SliverAppBar(
+      pinned: true,
+      floating: true,
+      backgroundColor: retroTheme.background,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+
+      flexibleSpace: FlexibleSpaceBar(
+        background: Stack(
+          children: [
+            Positioned.fill(
+              child: Opacity(
+                opacity: 0.05,
+                child: Image.asset(
+                  'assets/noise_texture.png',
+                  repeat: ImageRepeat.repeat,
+                  fit: BoxFit.none,
+                  package: 'flux_retro_ui',
+                ),
+              ),
+            ),
+
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 2,
+                color: retroTheme.outline.withValues(alpha: 0.3),
+              ),
+            ),
+
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      retroTheme.glareColor.withValues(alpha: 0.02),
+                      Colors.transparent,
+                      retroTheme.shadowColor.withValues(alpha: 0.1),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      title: Text(
+        title.toUpperCase(),
+        style: TextStyle(
+          color: retroTheme.surface,
+          fontFamily: 'RetroTitle',
+          package: 'flux_retro_ui',
+          fontSize: 26,
+          letterSpacing: 4,
+          fontWeight: FontWeight.bold,
+          shadows: [
+            Shadow(
+              color: retroTheme.shadowColor,
+              offset: Offset(2, 2),
+              blurRadius: 2,
+            ),
+          ],
+        ),
+      ),
+      actions: actions,
+      centerTitle: false,
+    );
+  }
+}
